37150c4bf32f20ccecd90be59cb96241f839ae5f
Postmodern <postmodern.mod3@gmail.com>
2011-02-27 20:25:02 -0800

    Added documentation.

--- lib/hexdump/extensions/file.rb
+++ lib/hexdump/extensions/file.rb
@@ -2,6 +2,27 @@ require 'hexdump/extensions/io'
 
 class File
 
+  #
+  # Hexdumps the contents of a file.
+  #
+  # @param [String] path
+  #   The path of the file.
+  #
+  # @param [#<<] output
+  #   The output to print the hexdump to.
+  #
+  # @yield [index,hex_segment,print_segment]
+  #   The given block will be passed the hexdump break-down of each segment.
+  #
+  # @yieldparam [Integer] index
+  #   The index of the hexdumped segment.
+  #
+  # @yieldparam [Array<String>] hex_segment
+  #   The hexadecimal-byte representation of the segment.
+  #
+  # @yieldparam [Array<String>] print_segment
+  #   The print-character representation of the segment.
+  #
   def self.hexdump(path,output=STDOUT,&block)
     self.open(path,'rb') do |file|
       file.hexdump(output,&block)
