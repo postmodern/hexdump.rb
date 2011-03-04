d0024e3aefa1114d41b8ce362ecfa6b2911920bc
Postmodern <postmodern.mod3@gmail.com>
2011-03-03 17:24:42 -0800

    Added additional formatting options to Hexdump.dump.

--- lib/hexdump/hexdump.rb
+++ lib/hexdump/hexdump.rb
@@ -8,6 +8,14 @@ module Hexdump
   # @param [Hash] options
   #   Additional options.
   #
+  # @option options [Symbol, Integer] :base (:hexadecimal)
+  #   The base to print bytes in. Supported bases include, `:hexadecimal`,
+  #   `:hex`, `16, `:decimal`, `:dec`, `10, `:octal`, `:oct`, `8`, `:binary`,
+  #   `:bin` and `2`.
+  #
+  # @option options [Boolean] :ascii (false)
+  #   Print ascii characters when possible.
+  #
   # @option options [#<<] :output (STDOUT)
   #   The output to print the hexdump to.
   #
@@ -27,11 +35,39 @@ module Hexdump
   #
   def Hexdump.dump(data,options={})
     output = options.fetch(:output,STDOUT)
+    base = options.fetch(:base,:hexadecimal)
+    ascii = options.fetch(:ascii,false)
+    byte_format = case base
+                  when :hexadecimal, :hex, 16
+                    "%.2x"
+                  when :decimal, :dec, 10
+                    "%.d"
+                  when :octal, :oct, 8
+                    "%.3o"
+                  when :binary, :bin, 2
+                    "%.8b"
+                  end
 
     index = 0
     offset = 0
     hex_segment = []
     print_segment = []
+
+    hex_byte = lambda { |byte|
+      if (ascii && (byte >= 0x20 && byte <= 0x7e))
+        byte.chr
+      else
+        byte_format % byte
+      end
+    }
+
+    print_byte = lambda { |byte|
+      if (byte >= 0x20 && byte <= 0x7e)
+        byte.chr
+      else
+        '.'
+      end
+    }
 
     segment = lambda {
       if block_given?
@@ -52,14 +88,8 @@ module Hexdump
         print_segment.clear
       end
 
-      hex_segment << ("%.2x" % b)
-
-      print_segment << case b
-                       when (0x20..0x7e)
-                         b.chr
-                       else
-                         '.'
-                       end
+      hex_segment << hex_byte[b]
+      print_segment << print_byte[b]
 
       offset += 1
 
