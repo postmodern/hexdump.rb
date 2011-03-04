347cee58a958a3d135d029d8406523d6d15805f7
Postmodern <postmodern.mod3@gmail.com>
2011-03-03 17:04:01 -0800

    Added basic specs.

--- spec/hexdump_spec.rb
+++ spec/hexdump_spec.rb
@@ -2,4 +2,30 @@ require 'spec_helper'
 require 'hexdump'
 
 describe Hexdump do
+  let(:bytes) { [104, 101, 108, 108, 111] }
+  let(:hex) { ['68', '65', '6c', '6c', '6f'] }
+  let(:print) { ['h', 'e', 'l', 'l', 'o'] }
+  let(:data) { print.join }
+
+  it "should append each line of the hexdump to the output" do
+    output = []
+
+    subject.dump(data,output)
+
+    output.length.should == 1
+    output[0].should include(hex.join(' '))
+  end
+
+  it "should yield the parts of each hexdump line to the given block" do
+    lines = []
+
+    subject.dump(data) do |index,hex,print|
+      lines << [index, hex, print]
+    end
+
+    lines.length.should == 1
+    lines[0][0].should == 0
+    lines[0][1].should == hex
+    lines[0][2].should == print
+  end
 end
