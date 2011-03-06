755b673c054af7118f00ce5fde307bfbb4371ca3
Postmodern <postmodern.mod3@gmail.com>
2011-03-05 21:43:54 -0800

    Allow Hexdump#hexdump to pass options.

--- lib/hexdump/hexdump.rb
+++ lib/hexdump/hexdump.rb
@@ -98,26 +98,12 @@ module Hexdump
   #
   # Hexdumps the object.
   #
-  # @param [#<<] output
-  #   The output to print the hexdump to.
-  #
-  # @yield [index,hex_segment,print_segment]
-  #   The given block will be passed the hexdump break-down of each segment.
-  #
-  # @yieldparam [Integer] index
-  #   The index of the hexdumped segment.
-  #
-  # @yieldparam [Array<String>] hex_segment
-  #   The hexadecimal-byte representation of the segment.
-  #
-  # @yieldparam [Array<String>] print_segment
-  #   The print-character representation of the segment.
-  #
-  # @return [nil]
+  # @param [Hash] options
+  #   Additional options.
   #
   # @see Hexdump.dump
   #
-  def hexdump(output=STDOUT,&block)
-    Hexdump.dump(self,:output => output,&block)
+  def hexdump(options={},&block)
+    Hexdump.dump(self,options,&block)
   end
 end
