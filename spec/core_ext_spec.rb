70e9f2789fd717544af8af3449dacd149373b0a8
Postmodern <postmodern.mod3@gmail.com>
2021-06-12 14:30:16 -0700

    Improved the spec descriptions for core_ext specs.

--- spec/core_ext_spec.rb
+++ spec/core_ext_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 require 'hexdump/core_ext'
 
 describe "Hexdump core_ext" do
-  it "should include Hexdump into String" do
+  it "should include Hexdump::Mixin into String" do
     expect(String).to include(Hexdump::Mixin)
   end
 
-  it "should include Hexdump into StringIO" do
+  it "should include Hexdump::Mixin into StringIO" do
     expect(StringIO).to include(Hexdump::Mixin)
   end
 
-  it "should include Hexdump into IO" do
+  it "should include Hexdump::Mixin into IO" do
     expect(IO).to include(Hexdump::Mixin)
   end
 
@@ -18,7 +18,7 @@ describe "Hexdump core_ext" do
     expect(File).to respond_to(:hexdump)
   end
 
-  it "should define Kernel.hexdump" do
+  it "should include Hexdump::ModuleMethods into ::Kernel" do
     expect(::Kernel).to include(Hexdump::ModuleMethods)
   end
 end
