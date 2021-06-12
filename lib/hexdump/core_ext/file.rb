6b09284c40c2591a40ba01b42fa8e320db85e0d1
Postmodern <postmodern.mod3@gmail.com>
2021-06-12 13:12:57 -0700

    Copy/paste the @option tags from Mixin#hexdump to File.hexdump.

--- lib/hexdump/core_ext/file.rb
+++ lib/hexdump/core_ext/file.rb
@@ -8,8 +8,20 @@ class File
   # @param [String] path
   #   The path of the file.
   #
-  # @param [Hash] options
-  #   Additional options.
+  # @param [Hash{Symbol => Object}] kwargs
+  #   Additional keyword arguments for {Format#initialize}.
+  #
+  # @option kwargs [#print] :output ($stdout)
+  #   The output to print the hexdump to.
+  #
+  # @option kwargs [:int8, :uint8, :char, :uchar, :byte, :int16, :int16_le, :int16_be, :int16_ne, :uint16, :uint16_le, :uint16_be, :uint16_ne, :short, :short_le, :short_be, :short_ne, :ushort, :ushort_le, :ushort_be, :ushort_ne, :int32, :int32_le, :int32_be, :int32_ne, :uint32, :uint32_le, :uint32_be, :uint32_ne, :int, :long, :long_le, :long_be, :long_ne, :uint, :ulong, :ulong_le, :ulong_be, :ulong_ne, :int64, :int64_le, :int64_be, :int64_ne, :uint64, :uint64_le, :uint64_be, :uint64_ne, :longlong, :longlong_le, :longlong_be, :longlong_ne, :ulonglong, :ulonglong_le, :ulonglong_be, :ulonglong_ne, :float, :float_le, :float_be, :float_ne, :double, :double_le, :double_be, :double_ne] :type (:byte)
+  #   The type to decode the data as.
+  #
+  # @option kwargs [Integer] :columns (16)
+  #   The number of bytes to dump for each line.
+  #
+  # @option kwargs [16, 10, 8, 2] :base (16)
+  #   The base to print bytes in.
   #
   # @see IO#hexdump
   #
