ff0bee0c27bfb92f0d1e779365be32abc120b959
Postmodern <postmodern.mod3@gmail.com>
2021-08-14 13:39:32 -0700

    Added Hexdump::Style::Rule to handle the styling and highlighting of strings.

---
+++ lib/hexdump/style/rule.rb
@@ -0,0 +1,111 @@
+require 'hexdump/style/ansi'
+
+require 'strscan'
+
+module Hexdump
+  class Style
+    #
+    # Represents a color highlighting rule.
+    #
+    # @api private
+    #
+    # @since 1.0.0
+    #
+    class Rule
+
+      # @return [ANSI, nil]
+      attr_reader :style
+
+      # @return [Hash{String,Regexp => ANSI}]
+      attr_reader :highlights
+
+      #
+      # Initializes the color.
+      #
+      # @param [Symbol, Array<Symbol>] style
+      #   The defualt style name(s). See {ANSI::STYLES}.
+      #
+      # @param [Hash{String,Regexp => Symbol,Array<Symbol>}, nil] highlights
+      #
+      def initialize(style=nil, highlights: nil)
+        @reset = ANSI::RESET
+        
+        @style = if style
+                   ANSI.new(style)
+                 end
+
+        @highlights = {}
+
+        if highlights
+          highlights.each do |pattern,style|
+            self.highlight(pattern,style)
+          end
+        end
+      end
+
+      #
+      # 
+      # @param [String, Regexp] pattern
+      #   The exact String to highlight or regular expression to highlight.
+      #
+      # @param [Symbol, Array<Symbol>] style
+      #   The style name(s). See {ANSI::STYLES}.
+      #
+      # @example
+      #   hexdump.style.numeric.highlight('00', :faint)
+      #
+      # @example
+      #   hexdump.style.index.highlight(/00$/, [:white, :bold])
+      #
+      # @api public
+      #
+      def highlight(pattern,style)
+        @highlights[pattern] = ANSI.new(style)
+      end
+
+      #
+      # Applies coloring/highlighting to a string.
+      #
+      # @param [String] string
+      #   The string to color/highlight.
+      #
+      # @return [String]
+      #   The colored the string.
+      #
+      def apply(string)
+        if @style || !@highlights.empty?
+          new_string = String.new
+          new_string << @style if @style
+
+          if !@highlights.empty?
+            scanner = StringScanner.new(string)
+
+            until scanner.eos?
+              matched = false
+
+              @highlights.each do |pattern,ansi|
+                if (match = scanner.scan(pattern))
+                  new_string << "#{ansi}#{match}#{@reset}#{@style}"
+                  matched = true
+                  break
+                end
+              end
+
+              unless matched
+                new_string << scanner.getch
+              end
+            end
+          else
+            new_string << string
+          end
+
+          new_string << @reset if @style
+          new_string
+        else
+          string
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/style/rule_spec.rb
@@ -0,0 +1,118 @@
+require 'spec_helper'
+require 'hexdump/style/rule'
+
+describe Hexdump::Style::Rule do
+  describe "#initialize" do
+    context "when no style is given" do
+      subject { described_class.new }
+
+      it "must set #style to nil" do
+        expect(subject.style).to be(nil)
+      end
+    end
+
+    context "when given a style" do
+      let(:style) { :bold }
+
+      subject { described_class.new(style) }
+
+      it "must initialize #style based on the given style" do
+        expect(subject.style).to       be_kind_of(Hexdump::Style::ANSI)
+        expect(subject.style.style).to eq(style)
+      end
+    end
+
+    context "when given no highlights: keyword argument" do
+      it "must initialize #highlights to {}" do
+        expect(subject.highlights).to eq({})
+      end
+    end
+
+    context "when given the highlights: keyword argument" do
+      let(:highlights) { {/[a-f]/ => :red, /[0-9]/ => :red} }
+
+      subject { described_class.new(highlights: highlights) }
+
+      it "must populate #highlights based on the given highlights" do
+        expect(subject.highlights.keys).to eq(highlights.keys)
+        expect(subject.highlights.values).to all(be_kind_of(Hexdump::Style::ANSI))
+      end
+    end
+  end
+
+  describe "#highlight" do
+    let(:highlights) { {'00' => :faint} }
+
+    subject { described_class.new(highlights: highlights) }
+
+    let(:pattern) { /[a-f]/ }
+    let(:style)   { [:green, :bold] }
+
+    before do
+      subject.highlight(pattern,style)
+    end
+
+    it "must add the pattern and style to #highlights" do
+      expect(subject.highlights[pattern]).to be_kind_of(Hexdump::Style::ANSI)
+      expect(subject.highlights[pattern].style).to eq(style)
+    end
+  end
+
+  describe "#apply" do
+    let(:string) { "00f0" }
+
+    let(:highlights) { {'00' => :faint, /[a-f]/ => :green} }
+
+    let(:reset)      { Hexdump::Style::ANSI::RESET }
+    let(:ansi_faint) { Hexdump::Style::ANSI::STYLES[:faint] }
+    let(:ansi_green) { Hexdump::Style::ANSI::STYLES[:green] }
+
+    context "when there is a default style" do
+      let(:style) { :bold }
+
+      subject { described_class.new(style) }
+
+      let(:ansi)  { subject.style.ansi }
+
+      it "must wrap the given string with the style's ANSI string and reset" do
+        expect(subject.apply(string)).to eq("#{ansi}#{string}#{reset}")
+      end
+
+      context "and there is additional highlighting rules" do
+        subject { described_class.new(style, highlights: highlights) }
+
+        it "must still wrap the given string with the style's ANSI string and reset" do
+          expect(subject.apply(string)).to start_with("#{ansi}")
+          expect(subject.apply(string)).to end_with("#{reset}")
+        end
+
+        it "must wrap the matched substrings with the ANSI string and reset" do
+          expect(subject.apply(string)).to include("#{ansi_faint}00#{reset}")
+          expect(subject.apply(string)).to include("#{ansi_green}f#{reset}")
+        end
+      end
+    end
+
+    context "when there is no default style" do
+      context "and there is no highlighting rules" do
+        it "must return the given string with no modifications" do
+          expect(subject.apply(string)).to eq(string)
+        end
+      end
+
+      context "and there is additional highlighting rules" do
+        let(:highlights) { {'00' => :faint, /[a-f]/ => :green} }
+
+        let(:ansi_faint) { Hexdump::Style::ANSI::STYLES[:faint] }
+        let(:ansi_green) { Hexdump::Style::ANSI::STYLES[:green] }
+
+        subject { described_class.new(highlights: highlights) }
+
+        it "must wrap the matched substrings with the ANSI string and reset" do
+          expect(subject.apply(string)).to include("#{ansi_faint}00#{reset}")
+          expect(subject.apply(string)).to include("#{ansi_green}f#{reset}")
+        end
+      end
+    end
+  end
+end
